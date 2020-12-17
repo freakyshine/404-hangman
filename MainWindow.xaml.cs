24a8fb6267e0c765a7f5ad76b100262da5803528
Lutz <lutzd11@emea.roche.com>
2020-12-17 08:24:12 +0100

    Added Game class, added game instance of Game, added Picture directory, added Grid

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,19 +12,27 @@ namespace Hangman
     /// </summary>
     public partial class MainWindow : Window
     {
+        Game game;
 
-        // This is the word the player should guess
-        private readonly string _word;
-
-        // Initially the player has 5 lives.
-        // Every wrong guess decreases the number of remaining lives.
-        private int _lives = 5;
-
+        /// <summary>
+        /// This is the MainWindow method which gets called at the start of the application.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+            NewGame();
+            game.UpdateHangman();
 
-            _word = WordLibrary.GetWord();
+            DataContext = game;
+
+        }
+
+        /// <summary>
+        /// This method is used to create a new game. Also to restart an already existing game.
+        /// </summary>
+        private void NewGame ()
+        {
+            game = new Game();
         }
 
         /**
@@ -38,5 +46,55 @@ namespace Hangman
          *  6) Implement "win" and "game over" notifications for the user and trigger them accordingly
          *  7) Be proud, have fun and play your own hangman game!
          */
+    }
+
+    /// <summary>
+    /// This class represents a game instance. Every game is its own instance of this class.
+    /// </summary>
+    class Game
+    {
+        /// <summary>
+        /// This is the word the player should guess.
+        /// </summary>
+        public readonly string _Word;
+        // Initially the player has 5 lives.
+        // Every wrong guess decreases the number of remaining lives.
+        private int _lives = 5;
+
+        public string ImagePath { get; private set; }
+
+
+        /// <summary>
+        /// This method updates the image path (ImagePath property) of the hangman.
+        /// </summary>
+        public void UpdateHangman ()
+        {
+            switch (_lives)
+            {
+                case 5:
+                    ImagePath = "live_5.png";
+                    break;
+                case 4:
+                    ImagePath = "live_4.png";
+                    break;
+                case 3:
+                    ImagePath = "live_3.png";
+                    break;
+                case 2:
+                    ImagePath = "live_2.png";
+                    break;
+                default:
+                    ImagePath = "live_1.png";
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// This constructor assignes the readonly to a random word from the WordLibrary class.
+        /// </summary>
+        public Game ()
+        {
+            _Word = WordLibrary.GetWord();
+        }
     }
 }
